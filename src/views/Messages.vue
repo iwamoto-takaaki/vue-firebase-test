c1561217d935e6e0ba7e07e1818afddd2555a5bc
Takaaki Iwamoto <iwamoto.takaaki@gmail.com>
2020-05-29 02:32:01 +0900

    add message test value

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
     #messages
-        h1 this is message page!
+        h1 this is message page!\
+        .message-list(v-for="message in messages")
+            p {{ message }}
 </template>
 <script lang="ts">
 import Vue from 'vue';
-export default Vue.extend({
-
-});
+export default class Message extends Vue {
+    public messages = ['hello world', 'hello everyone'];
+}
 </script>
