de6f88112ad1049d360d44e2357bb203dba5af7f
Takaaki Iwamoto <iwamoto.takaaki@gmail.com>
2020-06-02 12:45:13 +0900

    add new message butttom

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -3,6 +3,11 @@
         h1 this is message page!
         .message-list(v-for="message in messages")
             p {{ message.content }}
+        .message-form
+            form(@submit.prevent='addNewMessage')
+                label new message:
+                input(type='text' v-model='newMessage')
+                button(type='submmit') submit
 </template>
 <script lang="ts">
 import Vue from 'vue';
@@ -12,9 +17,21 @@ import messages, { Message } from '@/store/modules/messages';
 @Component
 export default class MessageView extends Vue {
     private messages = messages.messages;
+    private newMessage: string = '';
 
-    public async created() {
-        messages.add({content: 'hello evertone!'});
+    public async addNewMessage() {
+        await messages.add({content: this.newMessage});
+        this.newMessage = '';
     }
 }
 </script>
+<style lang="sass" scoped>
+#messages
+    .message-form
+        width: 500px;
+        background-color: #ddd
+        border: 1px solid #666
+        padding: 1rem
+        margin-left: auto
+        margin-right: auto
+</style>
